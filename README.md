faf9a1a6308f70ccd50d8057cc967e2abd660b0b
cleber <cleber@dronemapp.com>
2017-04-17 15:56:57 -0300

    add circle-ci support

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Powerlibs: AWS SNS
 
+[![CircleCI](https://circleci.com/gh/DroneMapp/powerlibs-aws-sns/tree/master.svg?style=svg)](https://circleci.com/gh/DroneMapp/powerlibs-aws-sns/tree/master)
+
 Libraries to facilitate publishing on Amazon SNS.
--- setup.py
+++ setup.py
@@ -1,3 +1,6 @@
+import re
+
+
 try:
     from setuptools import setup
 except ImportError:
@@ -5,8 +8,24 @@ except ImportError:
 
 version = '0.0.1'
 
+
+def pip_git_to_setuptools_git(url):
+    # git+https://github.com/DroneMapp/powerlibs-aws-sns.git@0.0.1
+    match = re.match(r'git\+https://github.com/(?P<organization>[^/]+)/(?P<repository>[^/]+).git@(?P<tag>.+)', url.strip())
+    if match:
+        url = 'http://github.com/{organization}/{repository}/tarball/master#egg={tag}'.format(
+            **match.groupdict()
+        )
+    return url
+
+requires = []
+dependency_links = []
 with open('requirements/production.txt') as requirements_file:
-    requires = [line for line in requirements_file]
+    for line in requirements_file:
+        if 'git+http' in line:
+            dependency_links.append(pip_git_to_setuptools_git(line))
+        else:
+            requires.append(line)
 
 with open('README.md') as f:
     readme = f.read()
@@ -27,6 +46,7 @@ setup(
     package_data={'': ['LICENSE', 'README.md']},
     include_package_data=True,
     install_requires=requires,
+    dependency_links=dependency_links,
     zip_safe=False,
     keywords='generic libraries',
     classifiers=(
